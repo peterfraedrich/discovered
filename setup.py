40bad3922c4e8d8ff42a48e4122dabcf5814db9d
Peter Fraedrich <peterfraedrich@gmail.com>
2017-12-20 18:57:23 -0500

    init

---
+++ setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name='dscovered',
+    version='0.0.1',
+    url='http://github.com/peterfraedrich/discovered',
+    license='MIT',
+    author='Peter Fraedrich',
+    author_email='peter.fraedrich@hexapp.net',
+    description='Redis-based service discovery for python',
+    packages=['discovered'],
+    platforms='any',
+    install_requires=[
+        'redis'
+    ],
+    keywords=[
+        'Redis',
+        'service discovery',
+        'consensus',
+        'discover'
+    ],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: MacOS',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
