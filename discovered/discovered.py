cb706dfef5cb4f181dd9e41fc00d47d366c51621
Peter Fraedrich <peterfraedrich@gmail.com>
2017-12-21 00:00:27 -0500

    fill out API

--- discovered/discovered.py
+++ discovered/discovered.py
@@ -31,13 +31,14 @@ class ServiceDiscovery:
         self.auto = auto
         self.sg = 'discovered:{}'.format(service_group)
         self.r = redis.StrictRedis(host=redis_host, port=redis_port, db=redis_db, password=redis_auth)
-        self.r.set('{}:{}'.format(self.sg, 'sd_init'), time.time())
+        self.r.set('{}:{}'.format(self.sg, '__init__'), time.time())
         return
 
-    def register_service(self, service_name, endpoint, auth_type='', auth_credentials='', backend='', description=''):
+    def register_service(self, service_name, endpoint, endpoint_type='', auth_type=None, auth_credentials=None, backend='', description=''):
         '''
         @param service_name str
         @param endpoint str
+        @param endpoint_type str
         @param auth_type str
         @param auth_credentials str
         @param backend str
@@ -46,15 +47,16 @@ class ServiceDiscovery:
         '''
         info = {
             'endpoint' : endpoint,
+            'endpoint_type' : endpoint_type,
             'auth_type' : auth_type,
             'auth_credentials' : auth_credentials,
             'backend' : backend,
             'description' : description
         }   
         self.r.hmset('{}:{}:{}'.format(self.sg, service_name, 'info'), info)
-        return
+        return info
 
-    def register_node(self, service_name, node_name, ip=None, fqdn=None, os=None, platform_name=None, arch=None, cpu_count=None, memory=None, last_boot=None):
+    def register_node(self, service_name, node_name, ip=None, fqdn=None, os=None, platform_name=None, arch=None, cpu_count=None, memory=None, last_boot=None, auto=True):
         '''
         @param service_name str
         @param node_name str
@@ -77,7 +79,7 @@ class ServiceDiscovery:
             'py_name' : __name__,
             'py_args' : sys.argv
         }
-        if self.auto:
+        if self.auto and auto:
             ip = []
             for name,iface in psutil.net_if_addrs().iteritems():
                 for obj in iface:
@@ -97,7 +99,7 @@ class ServiceDiscovery:
         self.r.hmset('{}:{}:{}:{}'.format(self.sg, service_name, 'nodes', node_name), info)
         if self.ttl != -1:
             self.r.expire('{}:{}:{}:{}'.format(self.sg, service_name, 'nodes', node_name), self.ttl)
-        return
+        return info
 
     def get_service(self, service_name):
         '''
@@ -115,3 +117,61 @@ class ServiceDiscovery:
         for node in self.r.scan_iter('{}:{}:{}:*'.format(self.sg, service_name, 'nodes')):
             nodes.append(self.r.hgetall(node))
         return nodes
+
+    def remove_node(self, service_name, node_name):
+        '''
+        @param service_name str
+        @param node_name str
+        deletes a node from the node list
+        '''
+        self.r.delete('{}:{}:{}:{}'.format(self.sg, service_name, 'nodes', node_name))
+        return node_name
+
+    def remove_service(self, service_name):
+        '''
+        @param service_name str
+        removes a service from the service group
+        '''
+        for n in self.r.scan_iter('{}:{}:*'.format(self.sg, service_name)):
+            self.r.delete(n)
+        #self.r.delete('{}:{}'.format(self.sg, service_name))
+        return service_name
+
+    def remove_service_group(self, service_group):
+        '''
+        @param service_group str
+        removes a service group
+        '''
+        for n in self.r.scan_iter('{}:*'.format(self.sg)):
+            self.r.delete(n)
+        return service_group
+
+    def list_services(self):
+        '''
+        @returns list
+        returns a list of all services in the service group
+        '''
+        scan = []
+        services = []
+        for s in self.r.scan_iter('{}:*'.format(self.sg)):
+            scan.append(s)
+        for i in scan:
+            x = i.split(':')
+            if x[2] not in services and x[2] != '__init__':
+                services.append(x[2])
+        return services
+
+    def list_service_groups(self):
+        '''
+        @returns list
+        returns a list of all service groups
+        '''
+        scan = []
+        groups = []
+        for s in self.r.scan_iter('{}:*'.format(self.sg)):
+            scan.append(s)
+        for i in scan:
+            x = i.split(':')
+            if x[1] not in groups:
+                groups.append(x[1])
+        return groups
